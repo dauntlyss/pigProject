c19a020c71a81ebd17c4fd61756c364d9d50ce68
Alyssa Harris <ah00214@my.westga.edu>
2021-06-10 21:12:12 -0400

    Built out swapWhoseTurn method

--- src/edu/westga/cs6910/pig/model/Game.java
+++ src/edu/westga/cs6910/pig/model/Game.java
@@ -174,10 +174,13 @@ public class Game implements Observable {
 	}
 
 	private void swapWhoseTurn() {
-		// TODO: Swap the players so that the other player becomes 
-		//       the current player.  Note that in order to access the
-		//		 object inside of the ObjectProperty, you'll need to use
-		//		 getValue() and setValue()
+		Player currentPlayer = this.currentPlayerObject.getValue();
+		if (currentPlayer == this.theHuman) {
+			currentPlayer = this.theComputer;
+		} else {
+			currentPlayer = this.theHuman;
+		}
+		this.currentPlayerObject.setValue(currentPlayer);
 
 	}
 
